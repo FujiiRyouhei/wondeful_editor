d198b43b1639c1f22b74b2fe2e33adaed4463777
藤井涼平 <>
2022-07-07 15:28:52 +0900

    users table の作成

--- db/migrate/20220627022451_devise_token_auth_create_users.rb
+++ db/migrate/20220627022451_devise_token_auth_create_users.rb
@@ -1,2 +1,12 @@
 class DeviseTokenAuthCreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :user do |t|
+      # user info
+      t.string :name
+      t.string :email
+      t.string :encrypted_password
+
+      t.timestamps
+    end
+  end
 end
