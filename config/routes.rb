bb8f5bc85aca27ef6ea805860e628a05c1411e21
藤井涼平 <>
2022-07-11 19:53:00 +0900

    no message

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth'
+  mount_devise_token_auth_for "User", at: "auth"
   as :user do
     # Define routes for User within this block.
   end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,10 +1,12 @@
+# frozen_string_literal: true
+
 # This file is copied to spec/ when you run 'rails generate rspec:install'
-require 'spec_helper'
-ENV['RAILS_ENV'] ||= 'test'
-require_relative '../config/environment'
+require "spec_helper"
+ENV["RAILS_ENV"] ||= "test"
+require_relative "../config/environment"
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
-require 'rspec/rails'
+require "rspec/rails"
 
 Faker::Config.locale = :ja
 
@@ -65,5 +67,5 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 
-  config.include FactoryBot::Syntax::methods
+  config.include FactoryBot::Syntax.methods
 end
