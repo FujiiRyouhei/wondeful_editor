bf407313c3b6fd97bd5b34b0102ea9ad408b90bd
藤井涼平 <>
2022-07-17 22:25:00 +0900

    no message

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  mount_devise_token_auth_for "User", at: "auth"
+  mount_devise_token_auth_for 'User', at: 'auth'
+  as :user do
+    # Define routes for User within this block.
+  end
   as :user do
     # Define routes for User within this block.
   end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -4,13 +4,15 @@ RSpec.describe Comment, type: :model do
   context "body を指定しているとき" do
     it "comment が作られる" do
       comment = FactoryBot.build(:comment)
-      binding.pry
       expect(comment).to be_valid
     end
   end
 
   context "body を指定していないとき" do
-    it "comment が作られる" do
+    it "comment の作成に失敗する" do
+      comment = FactoryBot.build(:comment, body: nil)
+      expect(comment).to be_invalid
+      expect(comment.errors.details[:body][0][:error]).to eq :blank
     end
   end
 end
