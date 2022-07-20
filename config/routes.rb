f7dd38b780f8f325d3d19bfccfd6c0acb7fa4270
藤井涼平 <>
2022-07-20 14:09:00 +0900

    記事に関するルーティングの実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,18 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  mount_devise_token_auth_for "User", at: "auth"
+  # resources :articles, path: 'api/v1/article'
+  # mount_devise_token_auth_for "User", at: "auth"
+  # resources :user
+
+
+  namespace :api do
+    namespace :v1 do
+      mount_devise_token_auth_for "User", at: "auth"
+      resources :articles
+    end
+  end
+
   as :user do
     # Define routes for User within this block.
   end
@@ -9,4 +20,6 @@ Rails.application.routes.draw do
     # Define routes for User within this block.
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # get '/api/v1/', to: 'article#'
 end
