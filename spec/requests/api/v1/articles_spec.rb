384079fa805eb58ac7a037cd2209e34c1b0db98e
藤井涼平 <>
2022-08-19 03:36:04 +0900

    no message

--- spec/requests/api/v1/articles_spec.rb
+++ spec/requests/api/v1/articles_spec.rb
@@ -35,19 +35,18 @@ RSpec.describe "/articles", type: :request do
   describe "GET /api_v1_articles" do
     subject { get(api_v1_articles_path) }
     # before { create_list(:article, 3)}
+
     let!(:article1) { create(:article, updated_at: 1.days.ago) }
     let!(:article2) { create(:article, updated_at: 2.days.ago) }
     let!(:article3) { create(:article) }
 
-
-
-    fit "article の一覧が取得できる" do
+    it "article の一覧が取得できる" do
       subject
       res = JSON.parse(response.body)
-      
+
       expect(res.length).to eq 3
-      expect( res[0].keys ).to eq ["id", "title", "updated_at", "user"]
-      expect(response).to have_http_status(200)
+      expect(res[0].keys).to eq ["id", "title", "updated_at", "user"]
+      expect(response).to have_http_status(:ok)
       expect(res.map {|d| d["id"] }).to eq [article3.id, article1.id, article2.id]
       expect(res[0]["user"].keys).to eq ["id", "name", "email"]
       # Article.create! valid_attributes
