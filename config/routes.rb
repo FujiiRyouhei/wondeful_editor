eb68d2e35264c466fee8fedd7e17d0bd491acc45
藤井涼平 <>
2022-09-13 14:20:54 +0900

    フロントエンドとの繋ぎ込みを実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,10 @@
-# frozen_string_literal: true
-
 Rails.application.routes.draw do
-  # resources :articles, path: 'api/v1/article'
-  # mount_devise_token_auth_for "User", at: "auth"
+  root to: "home#index"
+
   namespace :api do
     namespace :v1 do
       mount_devise_token_auth_for "User", at: "auth"
       resources :articles
     end
   end
-
-  as :user do
-    # Define routes for User within this block.
-  end
-  as :user do
-    # Define routes for User within this block.
-  end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-  # get '/api/v1/', to: 'article#'
 end
